9439bb7a33111d91008c956c15e04660e51a339c
shinochen <108452320+shinoshyun@users.noreply.github.com>
2022-12-16 14:59:53 +0800

    密碼修正

--- taipei-day-trip/route/user.py
+++ taipei-day-trip/route/user.py
@@ -8,7 +8,7 @@ mysql_connection = mysql.connector.connect(
     host='localhost',
     port='3306',
     user='root',
-    password='dumplings67',
+    password='password',
     database='attractions_data'
 )
 
